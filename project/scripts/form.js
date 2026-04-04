1f372d30d71e565aa8c2e4d48e848ac56f8ecfd1
AHHS25 <ahernandezserrato@byupathway.edu>
2026-04-04 15:59:26 -0600

    W05 updated

---
+++ project/scripts/form.js
@@ -0,0 +1,20 @@
+const products = [
+  { id: "fc-1888", name: "flux capacitor" },
+  { id: "fc-2050", name: "power laces" },
+  { id: "fs-1987", name: "time circuits" },
+  { id: "ac-2000", name: "low voltage reactor" },
+  { id: "jj-1969", name: "warp equalizer" }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const productSelect = document.querySelector("#productName");
+
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
+
+  document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+});
